32304a02ce519230fb0db4f0756c10118097109e
aidanLakhani <aidanlakhani10@gmail.com>
2025-04-29 10:56:06 -0400

    Initial commit with client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return <>{message}</>;
+}
+
+export default App;
