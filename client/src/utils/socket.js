40b7f910a71c29ba28c043d23ed881fc6070becd
aidanLakhani <aidanlakhani10@gmail.com>
2025-12-21 02:36:14 -0500

    Add concurrently dependency

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,7 +2,8 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = "https://localhost:5000";
-const URL = "http://" + window.location.hostname + ":5000";
+// const URL = "http://" + window.location.hostname + ":5000";
+const URL = "https://messaging-app-0ts7.onrender.com";
 
 export const socket = io(URL, {
   autoConnect: false,
